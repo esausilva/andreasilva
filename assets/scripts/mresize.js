ab333837cd293f93182481828ed18b4a43f8ffb8
Esau Silva <esausilva85@gmail.com>
2016-11-02 21:54:30 -0500

    Update 2016-11-03T02:54:29.127Z

--- assets/scripts/mresize.js
+++ assets/scripts/mresize.js
@@ -1,13 +1,4 @@
 /*
-== mresize jQuery plugin (event based element resize fn) == 
-Version: 1.0.1 
-Plugin URI: http://manos.malihu.gr/event-based-jquery-element-resize/ 
-Author: malihu
-Author URI: http://manos.malihu.gr
-License: MIT License (MIT)
-*/
-
-/*
 Copyright 2014 Manos Malihutsakis (email: manos@malihu.gr)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,44 +19,4 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-
-(function(factory){
-	
-	if(typeof define==="function" && define.amd){
-		define(["jquery"],factory); //AMD
-	}else if(typeof exports==="object"){
-		module.exports=factory; //Browserify
-	}else{
-		factory(jQuery); //globals
-	}
-	
-}(function($){
-	
-	$.event.special.mresize={
-		add:function(){
-			var el=$(this);
-			if(el.data("mresize")) return;
-			if(el.css("position")==="static") el.css("position","relative");
-			el
-				.append("<div class='resize' style='position:absolute; width:auto; height:auto; top:0; right:0; bottom:0; left:0; margin:0; padding:0; overflow:hidden; visibility:hidden; z-index:-1'><iframe style='width:100%; height:0; border:0; visibility:visible; margin:0' /><iframe style='width:0; height:100%; border:0; visibility:visible; margin:0' /></div>")
-				.data("mresize",{"w":el.width(),"h":el.height(),t:null,throttle:100})
-				.find(".resize iframe").each(function(){
-					$(this.contentWindow || this).on("resize",function(){
-						var d=el.data("mresize");
-						if(d.w!==el.width() || d.h!==el.height()){
-							if(d.t) clearTimeout(d.t);
-							d.t=setTimeout(function(){
-								el.triggerHandler("mresize");
-								d.w=el.width();
-								d.h=el.height();
-							},d.throttle);
-						}
-					});
-				});
-		},
-		remove:function(){
-			$(this).removeData("mresize").find(".resize").remove();
-		}
-	};
-	
-}));
+!function(i){"function"==typeof define&&define.amd?define(["jquery"],i):"object"==typeof exports?module.exports=i:i(jQuery)}(function(i){i.event.special.mresize={add:function(){var e=i(this);e.data("mresize")||("static"===e.css("position")&&e.css("position","relative"),e.append("<div class='resize' style='position:absolute; width:auto; height:auto; top:0; right:0; bottom:0; left:0; margin:0; padding:0; overflow:hidden; visibility:hidden; z-index:-1'><iframe style='width:100%; height:0; border:0; visibility:visible; margin:0' /><iframe style='width:0; height:100%; border:0; visibility:visible; margin:0' /></div>").data("mresize",{w:e.width(),h:e.height(),t:null,throttle:100}).find(".resize iframe").each(function(){i(this.contentWindow||this).on("resize",function(){var i=e.data("mresize");(i.w!==e.width()||i.h!==e.height())&&(i.t&&clearTimeout(i.t),i.t=setTimeout(function(){e.triggerHandler("mresize"),i.w=e.width(),i.h=e.height()},i.throttle))})}))},remove:function(){i(this).removeData("mresize").find(".resize").remove()}}});
