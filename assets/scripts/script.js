7c341dbeb3a6f7a8c5b8428ce4ff625080d3dbf2
Esau Silva <esausilva85@gmail.com>
2016-11-04 14:03:15 -0500

    detecting ie

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -60,6 +60,34 @@ $(document).ready(function ($) {
     $('#AnimationLnk')[0].click();
   });
 
+  function detectIE() {
+      var ua = window.navigator.userAgent;
+
+      var msie = ua.indexOf('MSIE ');
+      if (msie > 0) {
+          // IE 10 or older => return version number
+          return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+      }
+
+      var trident = ua.indexOf('Trident/');
+      if (trident > 0) {
+          // IE 11 => return version number
+          var rv = ua.indexOf('rv:');
+          return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+      }
+
+      var edge = ua.indexOf('Edge/');
+      if (edge > 0) {
+          // Edge (IE 12+) => return version number
+          return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+      }
+
+      // other browser
+      return false;
+  }
+
   //ScrollReveal
-  window.sr = ScrollReveal().reveal('#sr', { duration: 1000, mobile: true, origin: 'top', scale: 1 });
+  if (detectIE() > 9) {
+    window.sr = ScrollReveal().reveal('#sr', { duration: 1000, mobile: true, origin: 'top', scale: 1 });
+  }
 });
